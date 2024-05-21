d9cff8f3e2ab2e116fee8f057e9247bacceb5052
Josh Wooding <12938082+joshwooding@users.noreply.github.com>
2024-05-21 16:05:03 +0100

    Change TabstripNext's overflow menu to use a Menu instead of a Dropdown

--- packages/lab/src/tabs-next/OverflowMenu.tsx
+++ packages/lab/src/tabs-next/OverflowMenu.tsx
@@ -1,24 +1,32 @@
-import { forwardRef, ReactNode } from "react";
-import { Button, useForkRef } from "@salt-ds/core";
+import { forwardRef, ReactNode, MouseEvent, SyntheticEvent } from "react";
+import {
+  Button,
+  useForkRef,
+  Menu,
+  MenuTrigger,
+  MenuPanel,
+  MenuItem,
+  ButtonProps,
+} from "@salt-ds/core";
 import { OverflowMenuIcon } from "@salt-ds/icons";
 
 import { useOverflowContext, useOverflowMenu } from "@fluentui/react-overflow";
-import { Dropdown, DropdownProps } from "../dropdown";
 
 type TabValue = {
   value: string;
   label: ReactNode;
 };
 
-export interface OverflowMenuProps extends DropdownProps<TabValue> {
+export interface OverflowMenuProps extends ButtonProps {
   tabs: TabValue[];
+  onItemClick?: (event: SyntheticEvent, value: string) => void;
 }
 
-export const OverflowMenu = forwardRef<HTMLDivElement, OverflowMenuProps>(
+export const OverflowMenu = forwardRef<HTMLButtonElement, OverflowMenuProps>(
   function OverflowMenu(props, forwardedRef) {
-    const { tabs, ...rest } = props;
+    const { tabs, onItemClick, ...rest } = props;
     const { ref, overflowCount, isOverflowing } =
-      useOverflowMenu<HTMLDivElement>();
+      useOverflowMenu<HTMLButtonElement>();
     const handleRef = useForkRef(ref, forwardedRef);
     const itemVisibility = useOverflowContext(
       (context) => context.itemVisibility
@@ -28,20 +36,34 @@ export const OverflowMenu = forwardRef<HTMLDivElement, OverflowMenuProps>(
 
     if (!isOverflowing) return null;
 
+    const handleItemClick = (event: MouseEvent<HTMLDivElement>) => {
+      const value = event.currentTarget.dataset.value;
+      if (value) {
+        onItemClick?.(event, value);
+      }
+    };
+
     return (
-      <Dropdown<TabValue>
-        aria-label={`${overflowCount} more tabs`}
-        triggerComponent={
-          <Button aria-label="More tabs" variant="secondary" role="combobox">
-            <OverflowMenuIcon aria-hidden style={{ margin: 0 }} />
+      <Menu>
+        <MenuTrigger>
+          <Button
+            aria-label={`${overflowCount} more tabs`}
+            variant="secondary"
+            role="tab"
+            ref={handleRef}
+            {...rest}
+          >
+            <OverflowMenuIcon aria-hidden />
           </Button>
-        }
-        width="auto"
-        ref={handleRef}
-        source={tabList}
-        selected={null}
-        {...rest}
-      />
+        </MenuTrigger>
+        <MenuPanel>
+          {tabList.map(({ value, label }) => (
+            <MenuItem key={value} onClick={handleItemClick} data-value={value}>
+              {label}
+            </MenuItem>
+          ))}
+        </MenuPanel>
+      </Menu>
     );
   }
 );
